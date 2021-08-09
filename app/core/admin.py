945ce0ada3996379a96d55797487369299f9b614
E0003884 <31926976+E0003884@users.noreply.github.com>
2021-08-09 10:39:25 +0800

    Added working list tags endpoint

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,13 +4,15 @@ from django.utils.translation import gettext as _
 
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal Info'), {'fields': ('name',)}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Permissions'), {
+         'fields': ('is_active', 'is_staff', 'is_superuser')}),
         (_('Important Dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
@@ -19,5 +21,7 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2')
         }),
     )
-    
+
+
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag)
